6486140d19bab3caedcd437cea80035df6b1f148
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-02-04 15:36:17 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -22,16 +22,46 @@ https_server.listen("127.0.0.1", config["servers"["https"["port"]]]);
 
 if (config["cryptocurrencys"["bitcoin"["mining_enabled"]]] == 1) {
     
-    
+    const bitcoin_wallet-client = socket.client();
+    bitcoin_wallet-client.connect(config["cryptocurrencys"["bitcoin"["wallet"]]]);
+    bitcoin_wallet-client.send("getblocktemplate");
+    if (bitcoin_wallet-client.recv() != "") {
+        
+        Console.log("The bitcoin walet client was able to connect himself to the bitcoin wallet !");
+        
+    } else {
+        
+        Console.log("The bitcoin wallet client was not able to connect himself to the bitcoin wallet !");
+        
+    };
     
 };
 if (config["cryptocurrencys"["bitcoin-cash"["mining_enabled"]]] == 1) {
     
-    
+    const bitcoin-cash_wallet-client = socket.client();
+    Console.log("The Bitcoin Cash mining is enabled, the wallet client was created !");
+    bitcoin-cash_wallet-client.connect(config["cryptocurrencys"["bitcoin-cash"["wallet"]]]);
+    bitcoin-cash_wallet-client.send("getblocktemplate");
     
 };
 if (config["cryptocurrencys"["dash"["mining_enabled"]]] == 1) {
     
+    const dash_wallet-client = socket.client();
+    Console.log("The Dash mining is enabled, the wallet client was created !");
+    dash_wallet-client.connect(config["cryptocurrencys"["dash"["wallet"]]]);
+    dash_wallet-client.send("getblocktemplate");
+    if (dash_wallet-client.recv() != "") {
+        
+        Console.log("");
+        
+    } else {
+        
+        Console.log("");
+        
+    };
     
+} else {
+    
+    Console.log("The Dash mining is disabled, the wallet client was not created");
     
 };
